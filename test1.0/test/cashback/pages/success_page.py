690b1f445533470e7dfdc64da8e09407c272e094
AndyQa <andrej.kharin@gmail.com>
2020-06-29 16:50:30 +0100

    finish registration user'

--- test1.0/test/cashback/pages/success_page.py
+++ test1.0/test/cashback/pages/success_page.py
@@ -6,4 +6,6 @@ class SuccessPage(BasePage):
     def check_success_message(self, expected_result):
         success_message = self.browser.find_element(*SuccessPageLocators.SUCCESS_MESSAGE)
         actual_result = success_message.text
+        print(actual_result)
         assert actual_result == expected_result
+        #Todo разобраться с преобразованием строк что б они были в одном регистре
